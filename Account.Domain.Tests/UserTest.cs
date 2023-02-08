16871552933b19b58fbb7a2f234a13d465664bcb
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-02-08 09:55:17 -0300

    Add project files.

---
+++ Account.Domain.Tests/UserTest.cs
@@ -0,0 +1,60 @@
+using Account.Domain.Models;
+
+namespace Account.Domain.Tests
+{
+    public class UserTest
+    {
+        [Test]
+        [TestCase("name", "email@email.com", false)]
+        [TestCase("name", "", true)]
+        public void ShouldCreateUserAndCheckEmail(string name, string email, bool shouldThrowException)
+        {
+            // Act
+            // Arrange
+            // Assert
+            if (shouldThrowException)
+            {
+                Assert.Throws<ArgumentException>(() => new User(name, email));
+            }
+            else
+                Assert.Pass();
+        }
+
+        [Test]
+        [TestCase("name", "email@email.com", false)]
+        [TestCase("", "email@email.com", true)]
+        public void ShouldCreateUserAndCheckName(string name, string email, bool shouldThrowException)
+        {
+            // Act
+            // Arrange
+            // Assert
+            if (shouldThrowException)
+            {
+                Assert.Throws<ArgumentException>(() => new User(name, email));
+            }
+            else
+                Assert.Pass();
+        }
+
+        [Test]
+        [TestCase("name", "email@email.com", true)]
+        [TestCase("name", "email@email.com", false)]
+        public void ShouldCreateUserAndToogleIsActive(string name, string email, bool isActive)
+        {
+            // Arrange
+            var user = new User(name, email);
+
+            // Act
+            if (user.IsActive != isActive)
+                user.ToggleIsActive();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(user.Name, Is.EqualTo(name));
+                Assert.That(user.Email, Is.EqualTo(email));
+                Assert.That(user.IsActive, Is.EqualTo(isActive));
+            });
+        }
+    }
+}
---
+++ Account.Domain/Models/User.fs
@@ -0,0 +1,30 @@
+﻿namespace Account.Domain.Models
+
+    open System
+    open System.Text.RegularExpressions
+
+    type User(name: string, email: string) =
+        let isValidEmail (email: string) =
+            let regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")
+            regex.IsMatch(email)
+
+        let name =
+            if String.IsNullOrEmpty(name) then
+                raise (new ArgumentException("Name cannot be empty"))
+            else
+                name
+
+        let email =
+            if not (isValidEmail email) then
+                raise (new ArgumentException("Invalid email address"))
+            else
+                email
+
+        let mutable isActive = true
+
+        member this.Name with get() = name
+        member this.Email with get() = email
+        member this.IsActive with get() = isActive and set v = isActive <- v
+
+        member this.ToggleIsActive() =
+            isActive <- not isActive
