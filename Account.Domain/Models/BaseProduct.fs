4e4f4ac1c31fe8c6f6bbb691541e52c99af48905
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-02-09 09:15:26 -0300

    cleanup

--- Account.Domain/Models/BaseProduct.fs
+++ Account.Domain/Models/BaseProduct.fs
@@ -1,19 +1,19 @@
 ﻿namespace Account.Domain.Models
 
-    open System
+open System
 
-    [<AbstractClass>]
-    type public BaseProduct(name:string)=
-        let name =
-            if String.IsNullOrEmpty(name) then
-                raise (new ArgumentException("Name cannot be empty"))
-            else
-                name
+[<AbstractClass>]
+type public BaseProduct(name:string)=
+    let name =
+        if String.IsNullOrEmpty(name) then
+            raise (new ArgumentException("Name cannot be empty"))
+        else
+            name
                 
-        let mutable isActive = true
+    let mutable isActive = true
 
-        member this.Name with get() = name
-        member this.IsActive with get() = isActive and set v = isActive <- v
+    member this.Name with get() = name
+    member this.IsActive with get() = isActive and set v = isActive <- v
 
-        member this.ToggleIsActive() =
-            isActive <- not isActive
+    member this.ToggleIsActive() =
+        isActive <- not isActive
