7ce4e9aea1257fee48703d1610e6071e333b7341
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-02-10 09:42:17 -0300

    ShouldGetPriceRange

---
+++ Account.Business.ExchangeSchool.Tests/PricingTests.cs
@@ -0,0 +1,32 @@
+﻿using Account.Business.ExchangeSchool.Models;
+
+namespace Account.Business.ExchangeSchool.Tests
+{
+    [Parallelizable]
+    internal class PricingTests
+    {
+        [Test]
+        public void ShouldGetPriceRange()
+        {
+            // Arrange
+            var weekQuantity = 100;
+
+            var ranges = new List<PriceRange>
+            {
+                new PriceRange(10, 100, 5),
+                new PriceRange(101, 200, 4)
+            };
+
+            // Act
+            var selectedRange = Services.Services.FindRange(ranges.ToArray(), weekQuantity);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(selectedRange.RangeFrom, Is.EqualTo(10));
+                Assert.That(selectedRange.RangeTo, Is.EqualTo(100));
+                Assert.That(selectedRange.Price, Is.EqualTo(5));
+            });
+        }
+    }
+}
